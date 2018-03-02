bc53f8e57d10e7a62c7525237b8e7989df8ae395
NickArthur <nickarthur.night@gmail.com>
2018-03-01 23:29:14 -0600

    added kitties and nicks

--- common/models/product.js
+++ common/models/product.js
@@ -1,18 +1,17 @@
-'use strict';
+"use strict";
 
 module.exports = function(Product) {
-    // console.log(Product.getDataSource())
+  // console.log(Product.getDataSource())
 
-    Product.observe('access', function logQuery(ctx, next) {
-        console.log('access', ctx.options)
-        next();
+  Product.observe("access", function logQuery(ctx, next) {
+    console.log("access", ctx.options);
+    next();
+  });
+
+  Product.createOptionsFromRemotingContext = function(ctx) {
+    var base = this.base.createOptionsFromRemotingContext(ctx);
+    return Object.assign(base, {
+      fullCtx: ctx
     });
-
-    Product.createOptionsFromRemotingContext = function(ctx) {
-        var base = this.base.createOptionsFromRemotingContext(ctx);
-        return Object.assign(base, {
-            fullCtx: ctx,
-        });
-    };
-
+  };
 };
