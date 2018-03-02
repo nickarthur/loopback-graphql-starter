d5cbc41b4f6aa01c318361d3e2fbd5e45054afeb
NickArthur <nickarthur.night@gmail.com>
2018-03-02 00:10:36 -0600

    added yo to console output

--- server/server.js
+++ server/server.js
@@ -8,12 +8,12 @@ var app = (module.exports = loopback());
 app.start = function() {
   // start the web server
   return app.listen(function() {
-    app.emit("started");
+    app.emit("Yo! started");
     var baseUrl = app.get("url").replace(/\/$/, "");
-    console.log("Web server listening at: %s", baseUrl);
+    console.log("Yo! Web server listening at: %s", baseUrl);
     if (app.get("loopback-component-explorer")) {
       var explorerPath = app.get("loopback-component-explorer").mountPath;
-      console.log("Browse your REST API at %s%s", baseUrl, explorerPath);
+      console.log("Yo! Browse your REST API at %s%s", baseUrl, explorerPath);
     }
   });
 };
